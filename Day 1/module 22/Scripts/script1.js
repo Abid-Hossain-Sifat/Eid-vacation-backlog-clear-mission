d1ea1cd1fc1dc3be17a20667c1284070149d0dad
Abid-Hossain-Sifat <itzabid006@gmail.com>
2026-03-18 23:53:18 +0600

    Learn 8 DOM handling concepts

--- Day 1/module 22/Scripts/script1.js
+++ Day 1/module 22/Scripts/script1.js
@@ -16,16 +16,51 @@ const text = document.getElementById("damiText");
 console.log(text.innerText);
 
 
-const lists = document.getElementsByTagName("li");
-console.log(lists);
+const service = document.getElementById("services");
+console.log(service);
 
-for (let list of lists) {
-    console.log(list);
-    console.log(list.innerText);
-    list.innerText = "change list inner text";
-    console.log(list.innerText);
-}
+// for (let list of lists) {
+//     console.log(list);
+//     console.log(list.innerText);
+//     list.innerText = "change list inner text";
+//     console.log(list.innerText);
+// }
 const span = document.getElementById("count");
 console.log(span.innerText);
-span.innerText = parseInt(lists.length);
+span.innerText = parseInt(service.children.length);
 console.log(span.innerText);
+
+
+
+let food = document.getElementById("foodList");
+console.log(food.children);
+food.children.inner
+
+
+
+let addfood1 = document.createElement("li");
+addfood1.innerText = "Grilled Chicken";
+food.appendChild(addfood1);
+
+let addfood2 = document.createElement("li");
+addfood2.innerText = "Pizza";
+addfood2.className = "foreignFood";
+food.appendChild(addfood2);
+
+let addfood3 = document.createElement("li");
+addfood3.innerText = "Pasta";
+addfood3.className = "foreignFood";
+food.appendChild(addfood3);
+
+let addfood4 = document.createElement("li");
+addfood4.innerText = "Burger";
+addfood4.className = "foreignFood";
+food.appendChild(addfood4);
+
+let foreignFoods = document.getElementsByClassName("foreignFood");
+let foreignFoodCount = document.getElementById("foreignFoodCount");
+foreignFoodCount.innerText = parseInt(foreignFoods.length);
+
+
+let count = document.getElementById("foodCount");
+count.innerText = parseInt(food.children.length);
